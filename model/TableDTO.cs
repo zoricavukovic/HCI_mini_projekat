a16c3dc8fb11054194400fba4bf19eda4fd28035
Rajko Zagorac <rajkozgrc4@gmail.com>
2022-04-20 15:48:00 +0200

    dodat tabelarni prikaz

---
+++ model/TableDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjekatHCI.model
+{
+    public class TableDTO
+    {
+        public bool GDPselected { get; set; }
+        public String IntervalName { get; set; }
+        public String SelectedMaturity { get; set; }
+
+        public DateTime SelectedStartDate { get; set; }
+        public DateTime SelectedEndDate { get; set; }
+        public List<PerDataValue> DataToDisplay { get; set; }
+
+        public TableDTO(bool gDPselected, string intervalName, string selectedMaturity, DateTime selectedStartDate, DateTime selectedEndDate, List<PerDataValue> dataToDisplay)
+        {
+            GDPselected = gDPselected;
+            IntervalName = intervalName;
+            SelectedMaturity = selectedMaturity;
+            SelectedStartDate = selectedStartDate;
+            SelectedEndDate = selectedEndDate;
+            DataToDisplay = dataToDisplay;
+        }
+
+    }
+}
